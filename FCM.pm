7345d83df0677c9b2458d50c18366c20cc94777e
JERILJOSE96 <jeril96@gmail.com>
2018-02-26 14:23:22 +0530

    Create FCM.pm

---
+++ FCM.pm
@@ -0,0 +1,17 @@
+package BL::FCM;
+
+use Moose;
+
+has firebase_api_key => (
+   is  => 'ro',
+   isa => 'Str',
+   default => 'AIzaSyBf_s9Iz-V6cBvhxZT_7Z3YmncWWm7XAXQ'
+);
+
+has debug => (
+    is          => 'ro',
+    default     => 1,
+);
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
